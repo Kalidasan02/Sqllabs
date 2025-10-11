4389ae47e19489b8df4ccecc23bd06c4b8ffa8d6
Kalidasan S <kalidasansmail002@gmail.com>
2025-10-11 10:55:42 +0530

    Commit

---
+++ work1.sql
@@ -0,0 +1,4 @@
+SELECT * FROM students WHERE age > '20';
+SELECT * FROM students WHERE department IN ('Computer Science', 'Physics');
+SELECT * FROM students WHERE grade = '90';
+SELECT * FROM students WHERE grade BETWEEN '70' AND '90';
